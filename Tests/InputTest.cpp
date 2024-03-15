d915cf723dbe39700e5c5d71b6a78282abac90f0
TomDeWildeee <tomdewilde0@gmail.com>
2024-03-15 01:40:41 +0100

    Restructured test files into their own folders + fixed illegal input tests so it works now

--- Tests/InputTest.cpp
+++ Tests/InputTest.cpp
@@ -16,62 +16,70 @@ protected:
 
     PrintingSystem printsystem;
 };
+
+
+
 TEST_F(PrintingSystemTest, inputsyntaxerrors){
-    ASSERT_TRUE(DirectoryExists("testXMLs"));
+    ASSERT_TRUE(DirectoryExists("testXMLs/inputsyntaxerrors"));
     ofstream myfile;
     ImportEnum importresult;
     int counter = 1;
-    string filename = "testXMLs/xmlsyntaxerror" + ToString(counter) + ".xml";
+    string filename = "testXMLs/inputsyntaxerrors/xmlsyntaxerror" + ToString(counter) + ".xml";
     string errorfilename;
     while(FileExists(filename)){
-        myfile.open("testXMLs/xmlError.txt");
+        myfile.open("testXMLs/inputsyntaxerrors/xmlError.txt");
         importresult = PrintingSystemImporter::importPrintingSystem(filename.c_str(),myfile,printsystem);
         myfile.close();
         EXPECT_TRUE(importresult == ImportError);
-        errorfilename = "testXMLs/xmlsyntaxerror" + ToString(counter) + ".txt";
-        EXPECT_TRUE(FileCompare("testXMLs/xmlError.txt", errorfilename));
+        errorfilename = "testXMLs/inputsyntaxerrors/xmlsyntaxerror" + ToString(counter) + ".txt";
+        EXPECT_TRUE(FileCompare("testXMLs/inputsyntaxerrors/xmlError.txt", errorfilename));
         counter +=1;
-        filename = "testXMLs/xmlsyntaxerror" + ToString(counter) + ".xml";
+        filename = "testXMLs/inputsyntaxerrors/xmlsyntaxerror" + ToString(counter) + ".xml";
     }
     EXPECT_TRUE(counter == 5);
 }
-/*
+
+
+
+std::string illegalInputDirectory = "testXMLs/illegalinput";
+
 TEST_F(PrintingSystemTest, inputillegalnumbers){
-    ASSERT_TRUE(DirectoryExists("testXMLs"));
+    ASSERT_TRUE(DirectoryExists(illegalInputDirectory));
     ofstream myfile;
     ImportEnum importresult;
     int counter = 1;
-    string filename = "testXMLs/inputillegal" + ToString(counter) + ".xml";
+    string filename = illegalInputDirectory + "/inputillegal" + ToString(counter) + ".xml";
     string errorfilename;
     while(FileExists(filename)){
-        myfile.open("testXMLs/inputillegal.txt");
+        myfile.open(illegalInputDirectory + "/inputillegal.txt");
         importresult = PrintingSystemImporter::importPrintingSystem(filename.c_str(),myfile,printsystem);
         myfile.close();
         EXPECT_TRUE(importresult == ImportError);
-        errorfilename = "testXMLs/inputillegal" + ToString(counter) + ".txt";
-        EXPECT_TRUE(FileCompare("testXMLs/inputillegal.txt", errorfilename));
-        counter +=1;
-        filename = "testXMLs/inputillegal" + ToString(counter) + ".xml";
+        errorfilename = illegalInputDirectory + "/inputillegal" + ToString(counter) + ".txt";
+        EXPECT_TRUE(FileCompare(illegalInputDirectory + "/inputillegal.txt", errorfilename));
+        counter += 1;
+        filename = illegalInputDirectory + "/inputillegal" + ToString(counter) + ".xml";
     }
-    EXPECT_TRUE(counter == 5);
+    EXPECT_TRUE(counter == 4);
 }
- */
+
+
+
 TEST_F(PrintingSystemTest, inputlegal){
-    ASSERT_TRUE(DirectoryExists("testXMLs"));
+    ASSERT_TRUE(DirectoryExists("testXMLs/legalinput"));
     ofstream myfile;
     ImportEnum importresult;
     int counter = 1;
-    string filename = "testXMLs/inputlegal" + ToString(counter) + ".xml";
+    string filename = "testXMLs/legalinput/inputlegal" + ToString(counter) + ".xml";
     string errorfilename;
     while(FileExists(filename)){
-        myfile.open("testXMLs/xmlError.txt");
+        myfile.open("testXMLs/legalinput/xmlError.txt");
         importresult = PrintingSystemImporter::importPrintingSystem(filename.c_str(),myfile,printsystem);
         myfile.close();
         EXPECT_TRUE(importresult == ImportSuccess);
         counter +=1;
         if(counter == 5){break;}
-        std::cout << filename << std::endl;
-        filename = "testXMLs/inputlegal" + ToString(counter) + ".xml";
+        filename = "testXMLs/legalinput/inputlegal" + ToString(counter) + ".xml";
     }
     EXPECT_TRUE(counter == 5);
 }

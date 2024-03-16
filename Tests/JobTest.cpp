cdc70f850b8c35c817e742d9707b7c50ccfcbec7
TomDeWildeee <tomdewilde0@gmail.com>
2024-03-16 02:18:46 +0100

    Added Job & Device constructor tests

---
+++ Tests/JobTest.cpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+#include "../Job.h"
+
+using namespace std;
+class JobTest: public ::testing::Test{
+protected:
+    void SetUp() override{
+    }
+    void TearDown() override {
+    }
+};
+
+TEST_F(JobTest, ConstructorTest) {
+    auto* job = new Job(100, 5, "Jan");
+    EXPECT_NE(job->getJobNR(), 10);
+    EXPECT_EQ(job->getJobNR(), 100);
+    EXPECT_NE(job->getPageCount(), 10);
+    EXPECT_EQ(job->getPageCount(), 5);
+    EXPECT_NE(job->getUserName(), "Tom");
+    EXPECT_EQ(job->getUserName(), "Jan");
+    delete job;
+}
